d06044af1e533f96abf645ee51fd19aa2b6d8d35
Lorenzo Serloni <S1109161@studenti.univpm.it>
2023-10-16 19:38:06 +0200

    changes

--- src/app/User/navbar.tsx
+++ src/app/User/navbar.tsx
@@ -1,5 +1,5 @@
-'use client'
-import React, { useState } from 'react';
+"use client";
+import React, { useState } from "react";
 
 export default function Navbar() {
   const [selected, setSelected] = useState(true);
@@ -7,43 +7,52 @@ export default function Navbar() {
   const [selected3, setSelected3] = useState(false);
 
   return (
-    <div className="flex w-full h-20 justify-evenly items-center bg-sky-400">
-        <div className=" font-bold w-30 h-max">
-            <button
-                className={`text-2xl bold ${selected ? 'text-sky-200 underline decoration-sky-200 brightness-200 drop-shadow-2xl' : 'text-sky-200'}`}
-                onClick={() => {
-                    setSelected(true);
-                    setSelected2(false);
-                    setSelected3(false);
-                }}
-            >
-            Home
-            </button>
-        </div>
-        <div className=" font-bold w-30 h-max">
+    <div
+      className="flex w-full h-16 justify-evenly items-center"
+      style={{ background: "linear-gradient(to bottom, #1E90FF,#1a8cff)" }}
+    >
+      <div className=" font-bold w-30 h-max">
         <button
-            className={`text-2xl ${selected2 ? 'text-sky-200 shadow-lg underline decoration-sky-200 drop-shadow-2xl' : 'text-sky-200'}`}
-            onClick={() => {
-                setSelected(false);
-                setSelected2(true);
-                setSelected3(false);
-            }}
+          className={`text-2xl bold ${
+            selected ? "text-white underline decoration-" : "text-white"
+          }`}
+          onClick={() => {
+            setSelected(true);
+            setSelected2(false);
+            setSelected3(false);
+          }}
+        >
+          Home
+        </button>
+      </div>
+      <div className=" font-bold w-30 h-max">
+        <button
+          className={`text-2xl ${
+            selected2 ? "text-white underline decoration-" : "text-white"
+          }`}
+          onClick={() => {
+            setSelected(false);
+            setSelected2(true);
+            setSelected3(false);
+          }}
         >
           Challenges
         </button>
-        </div>
-        <div className=" font-bold w-30 h-max">
-            <button
-                className={`text-2xl ${selected3 ? 'text-sky-200 underline decoration-sky-200 brightness-200 drop-shadow-2xl' : 'text-sky-200 '}`}
-                onClick={() => {
-                    setSelected(false);
-                    setSelected2(false);
-                    setSelected3(true);
-                }}
-            >
-                Profile
-            </button>
-        </div>
+      </div>
+      <div className=" font-bold w-30 h-max">
+        <button
+          className={`text-2xl ${
+            selected3 ? "text-white underline decoration-" : "text-white"
+          }`}
+          onClick={() => {
+            setSelected(false);
+            setSelected2(false);
+            setSelected3(true);
+          }}
+        >
+          Profile
+        </button>
+      </div>
     </div>
   );
 }
