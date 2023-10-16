b0de2a5625bef8515f1cdc69fd9c8d9869cf0ee1
Lorenzo Serloni <S1109161@studenti.univpm.it>
2023-10-16 19:11:15 +0200

    changes

--- src/app/User/navbar.tsx
+++ src/app/User/navbar.tsx
@@ -7,39 +7,43 @@ export default function Navbar() {
   const [selected3, setSelected3] = useState(false);
 
   return (
-    <div className="flex justify-between items-center bg-sky-400">
-      <div className="flex justify-between items-center">
+    <div className="flex w-full h-20 justify-evenly items-center bg-sky-400">
+        <div className=" font-bold w-30 h-max">
+            <button
+                className={`text-2xl bold ${selected ? 'text-sky-200 underline decoration-sky-200 brightness-200 drop-shadow-2xl' : 'text-sky-200'}`}
+                onClick={() => {
+                    setSelected(true);
+                    setSelected2(false);
+                    setSelected3(false);
+                }}
+            >
+            Home
+            </button>
+        </div>
+        <div className=" font-bold w-30 h-max">
         <button
-          className={`text-xl ml-5 ${selected ? 'text-sky-200' : 'text-sky-100'}`}
-          onClick={() => {
-            setSelected(true);
-            setSelected2(false);
-            setSelected3(false);
-          }}
-        >
-          Home
-        </button>
-        <button
-          className={`text-xl ml-5 ${selected2 ? 'text-sky-200' : 'text-sky-100'}`}
-          onClick={() => {
-            setSelected(false);
-            setSelected2(true);
-            setSelected3(false);
-          }}
+            className={`text-2xl ${selected2 ? 'text-sky-200 shadow-lg underline decoration-sky-200 drop-shadow-2xl' : 'text-sky-200'}`}
+            onClick={() => {
+                setSelected(false);
+                setSelected2(true);
+                setSelected3(false);
+            }}
         >
           Challenges
         </button>
-        <button
-          className={`text-xl ml-5 ${selected3 ? 'text-sky-200' : 'text-sky-100'}`}
-          onClick={() => {
-            setSelected(false);
-            setSelected2(false);
-            setSelected3(true);
-          }}
-        >
-          Profile
-        </button>
-      </div>
+        </div>
+        <div className=" font-bold w-30 h-max">
+            <button
+                className={`text-2xl ${selected3 ? 'text-sky-200 underline decoration-sky-200 brightness-200 drop-shadow-2xl' : 'text-sky-200 '}`}
+                onClick={() => {
+                    setSelected(false);
+                    setSelected2(false);
+                    setSelected3(true);
+                }}
+            >
+                Profile
+            </button>
+        </div>
     </div>
   );
 }
