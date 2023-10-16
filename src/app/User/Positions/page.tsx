4938d42dc597e86d98440617af37e0f6ede452a5
Lorenzo Serloni <S1109161@studenti.univpm.it>
2023-10-16 22:39:18 +0200

    changes

--- src/app/User/Positions/page.tsx
+++ src/app/User/Positions/page.tsx
@@ -10,7 +10,7 @@ export default function Positions() {
       <Heading />
       <div className="flex justify-evenly items-center">
         <div className="grid grid-cols-3 gap-28 ">
-          <div className="max-w-lg rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
+          <div className="max-w-2xl rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
             <Image src="/zucchetti.png" alt="photo" width={1000} height={1000} />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -31,7 +31,7 @@ export default function Positions() {
               </span>
             </div>
           </div>
-          <div className="max-w-lg rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
+          <div className="max-w-2xl rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
             <Image src="/zucchetti.png" alt="photo" width={1000} height={1000} />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
@@ -52,7 +52,7 @@ export default function Positions() {
               </span>
             </div>
           </div>
-          <div className="max-w-lg rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
+          <div className="max-w-2xl rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
             <Image src="/zucchetti.png" alt="photo" width={1000} height={1000} />
             <div className="px-6 py-4">
               <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
