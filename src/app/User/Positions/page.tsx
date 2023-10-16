26a8fdad265ad324be0ad1e7bb40679f57601d2d
Lorenzo Serloni <S1109161@studenti.univpm.it>
2023-10-16 22:05:27 +0200

    changes

--- src/app/User/Positions/page.tsx
+++ src/app/User/Positions/page.tsx
@@ -1,3 +1,103 @@
-export default function Positions() {
+import Navbar from "../navbar";
+import Image from "next/image";
 
+export default function Positions() {
+  return (
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center py-28 gap-4">
+        <h1 className="2xl:text-5xl lg:text-3xl extrabold text-sky-700">
+          Our Free Positions
+        </h1>
+        <h1 className="2xl:text-xl lg:text-lg extrabold text-black-500 py-1">
+          Explore our career opportunities
+        </h1>
+        <div className="md:flex md:items-center mb-6">
+          <div className="md:w-1/3">
+            <label
+              className="block text-gray-500 font-bold md:text-right mb-1 md:mb-0 pr-4"
+              htmlFor="inline-full-name"
+            >
+              Full Name
+            </label>
+          </div>
+          <div className="md:w-2/3">
+            <input
+              className="bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500"
+              id="inline-full-name"
+              type="text"
+              value="Jane Doe"
+            />
+          </div>
+        </div>
+      </div>
+      <div className="flex justify-evenly items-center">
+        <div className="grid grid-cols-3 gap-28 ">
+          <div className="max-w-lg rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
+            <Image src="/zucchetti.png" alt="photo" width={1000} height={1000} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
+                eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+          <div className="max-w-lg rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
+            <Image src="/zucchetti.png" alt="photo" width={1000} height={1000} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
+                eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+          <div className="max-w-lg rounded overflow-hidden shadow-lg px-6 py-4 justify-self-stretch">
+            <Image src="/zucchetti.png" alt="photo" width={1000} height={1000} />
+            <div className="px-6 py-4">
+              <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+              <p className="text-gray-700 text-base">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis
+                eaque, exercitationem praesentium nihil.
+              </p>
+            </div>
+            <div className="px-6 pt-4 pb-2">
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #photography
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #travel
+              </span>
+              <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                #winter
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
