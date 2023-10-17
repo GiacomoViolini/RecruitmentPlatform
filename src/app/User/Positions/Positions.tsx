a93c084e2b4885ac1285577cb24d1261f4de64f5
Lorenzo Serloni <S1109161@studenti.univpm.it>
2023-10-17 19:12:49 +0200

    changes

--- src/app/User/Positions/Positions.tsx
+++ src/app/User/Positions/Positions.tsx
@@ -2,13 +2,14 @@
 import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import { createClient } from "@supabase/supabase-js";
+import logo from "/public/logo.svg";
 
 interface Position {
     id: number;
     title: string;
     images: string[];
     description: string;
-    role: string;
+    ral: string;
     position: string;
     yearsOfExperience: number;
     typeOfPositions: string;
@@ -32,35 +33,45 @@ export default function Positions() {
 
         fetchData();
     }, []);
-
     return (
         <div className="flex justify-evenly items-center ">
-            <div className="grid grid-cols-3 gap-28 ">
+            <div className="grid grid-cols-3 gap-28 pb-8">
                 {positions.map((position) => (
                     <div
                         key={position.id}
-                        className="max-w-2xl rounded overflow-hidden shadow px-6 py-4 justify-self-stretch"
+                        className="max-w-2xl rounded overflow-hidden shadow px-6 py-4 bg-white justify-self-stretch"
                     >
-                        {position.images && position.images.map((image) => (
-                            <Image key={image} src={image} alt="photo" width={1000} height={1000} />
-                        ))}
-                        <div className="px-6 py-4">
-                            <div className="font-bold text-xl mb-2">{position.title}</div>
-                            <p className="text-gray-700 text-base">{position.description}</p>
+                        <div className="flex flex-row justify-between items-center pt-2">
+                            <div className="flex flex-row justify-start items-center gap-4 pt-1">
+                                <Image src={logo} alt="logo" width={30} height={30} className="mb-2"/>
+                                <div className="font-bold text-xl mb-2">{position.title}</div>
+                            </div>
+                            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                                Apply
+                            </button>
                         </div>
-                        <div className="px-6 pt-4 pb-2">
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                                {position.role}
-                            </span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                                {position.position}
-                            </span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                                {position.yearsOfExperience} years of experience
-                            </span>
-                            <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-                                {position.typeOfPositions}
-                            </span>
+                        <hr className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-sky-500 to-transparent opacity-25 dark:opacity-50" />
+                            {position.images && position.images.map((image) => (
+                                <Image key={image} src={image} alt="photo" width={100} height={100} />
+                            ))}
+                        <div className="py-4 text-left">
+                            <div className="px-6 py-4">
+                                <div className="text-bold text-mb mb-4">{position.description}</div>
+                            </div>
+                            <div className="px-6 pt-4 pb-2">
+                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                    {position.ral}
+                                </span>
+                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                    {position.position}
+                                </span>
+                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                    {position.yearsOfExperience} years of experience
+                                </span>
+                                <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+                                    {position.typeOfPositions}
+                                </span>
+                            </div>
                         </div>
                     </div>
                 ))}
--- src/app/User/Positions/page.tsx
+++ src/app/User/Positions/page.tsx
@@ -2,21 +2,6 @@ import Navbar from "../navbar";
 import Heading from "./heading";
 import PositionsFromSupabase from "./Positions";
 
-interface Position {
-  id: number;
-  title: string;
-  images: string[];
-  description: string;
-  role: string;
-  position: string;
-  yearsOfExperience: number;
-  typeOfPositions: string;
-}
-
-interface PositionsProps {
-  positions: Position[];
-}
-
 export default function Positions() {
   return (
     <div className="bg-slate-50 h-full">
