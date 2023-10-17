2098d565d42c662fa1b3ebfa054af99a94d08c36
GiacomoViolini <giacvio@gmail.com>
2023-10-17 18:39:19 +0200

    Challenges

--- src/app/User/Challenge/ChallengesList.tsx
+++ src/app/User/Challenge/ChallengesList.tsx
@@ -1,11 +1,12 @@
 import Image from "next/image";
+import Link from "next/link";
 
 async function getChallenges() {
-  const res = await fetch("http://localhost:4000/challenges",{
+  const res = await fetch("http://localhost:4000/challenges", {
     next: {
       revalidate: 120,
     },
-  })
+  });
   return res.json();
 }
 
@@ -14,24 +15,34 @@ export default async function ChallengesList() {
 
   return (
     <>
-      {challenges.map((c: { title: string; desc: string; img: string }) => (
-        <div
-          key={c.title}
-          className="card bg-white flex my-12 mx-auto h-48 w-9/12 rounded-xl overflow-hidden shadow border-transparent border-2"
-        >
-          <Image
-            src={"/" + c.img}
-            alt={c.title}
-            className="object-fit"
-            width={300}
-            height={70}
-          />
-          <div className="ml-5 mt-5">
-            <h2 className="text-2xl mb-3 font-bold text-sky-800">{c.title}</h2>
-            <h3 className="text-lg text-gray-500">{c.desc.slice(0, 300)}...</h3>
+      {challenges.map(
+        (c: { title: string; desc: string; img: string; id: string }) => (
+          <div
+            key={c.title}
+            className="bg-white my-12 mx-auto h-48 w-9/12 rounded-xl overflow-hidden shadow border-transparent border-2 hover:shadow-xl hover:border-sky-800"
+          >
+            <Link href={`Challenge/${c.id}`}>
+              <div className="flex">
+                <Image
+                  src={"/" + c.img}
+                  alt={c.id}
+                  className="object-fit"
+                  width={300}
+                  height={70}
+                />
+                <div className="ml-5 mt-5">
+                  <h2 className="text-2xl mb-3 font-bold text-sky-800">
+                    {c.title}
+                  </h2>
+                  <h3 className="text-lg text-gray-500">
+                    {c.desc.slice(0, 300)}...
+                  </h3>
+                </div>
+              </div>
+            </Link>
           </div>
-        </div>
-      ))}
+        )
+      )}
       {challenges.length === 0 && (
         <h1 className="text-2xl text-center font-semibold text-gray-800">
           No challenges yet
--- src/app/User/Challenge/[id]/page.tsx
+++ src/app/User/Challenge/[id]/page.tsx
@@ -1,8 +1,12 @@
 import Navbar from "../../navbar";
+import Image from "next/image";
 
 interface ChallengeProps {
   params: {
     id: string;
+    title: string;
+    img: string;
+    desc: string;
   };
 }
 
@@ -17,12 +21,32 @@ async function getChallenge(id: string) {
 }
 
 export default async function challenge({ params: { id } }: ChallengeProps) {
-  const ticket = await getChallenge(id);
+  const challenge = await getChallenge(id);
   return (
     <div className="bg-light-bg h-full pb-3 flex flex-col items-center">
       <Navbar />
-      <div className="bg-cyan-600 p-20 mt-20">
-        <h1 className="text-black">aaaaaaaaaaa{id}</h1>
+      <div
+        key={challenge.title}
+        className="bg-white flex my-32 mx-auto h-96 w-10/12 rounded-xl overflow-hidden shadow border-transparent border-2"
+      >
+        <Image
+          src={"/" + challenge.img}
+          alt={id}
+          className="object-contain"
+          width={500}
+          height={96}
+        />
+        <div className="ml-5 mt-5 flex flex-col items-center justify-between">
+          <div>
+            <h2 className="text-2xl mb-3 font-bold text-sky-800">
+              {challenge.title}
+            </h2>
+            <h3 className="text-lg text-gray-500">{challenge.desc}</h3>
+          </div>
+          <button className="bg-sky-500 px-8 py-2 mb-3 rounded-md text-white text-xl font-semibold hover:bg-sky-400">
+            Unisciti!
+          </button>
+        </div>
       </div>
     </div>
   );
