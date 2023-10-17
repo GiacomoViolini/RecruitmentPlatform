8bf4a653ae56db233dbb09feb7858511cdcf9e01
GiacomoViolini <giacvio@gmail.com>
2023-10-17 18:00:46 +0200

    config

--- src/app/User/Challenge/[id]/page.tsx
+++ src/app/User/Challenge/[id]/page.tsx
@@ -7,7 +7,7 @@ interface ChallengeProps {
 }
 
 async function getChallenge(id: string) {
-  const res = await fetch("http://localhost:4000/challenges", {
+  const res = await fetch("http://localhost:4000/challenges/" + id, {
     next: {
       revalidate: 120,
     },
@@ -17,11 +17,13 @@ async function getChallenge(id: string) {
 }
 
 export default async function challenge({ params: { id } }: ChallengeProps) {
-    const ticket = await getChallenge(id);
-    return (
-        <>
-            <Navbar />
-            <div>{id}</div>
-        </>
-    );
+  const ticket = await getChallenge(id);
+  return (
+    <div className="bg-light-bg h-full pb-3 flex flex-col items-center">
+      <Navbar />
+      <div className="bg-cyan-600 p-20 mt-20">
+        <h1 className="text-black">aaaaaaaaaaa{id}</h1>
+      </div>
+    </div>
+  );
 }
