225e2978a6465b72c4b533e620f130246c5b5caf
GiacomoViolini <giacvio@gmail.com>
2023-10-22 23:30:21 +0200

    deploy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output : "export",
     images: {
         domains: ['iomnamjdrrofldfdnwfc.supabase.co'], // Add your Supabase hostname here
       },
