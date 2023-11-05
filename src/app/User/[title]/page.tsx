50758d46e2cff4dd7970f0d5754fcd0bdca1e581
DamianoLuzi <luzi.dami03@gmail.com>
2023-11-05 18:53:23 +0100

    changes

--- src/app/User/[title]/page.tsx
+++ src/app/User/[title]/page.tsx
@@ -8,11 +8,16 @@ interface PositionParams {
 
 
 export default function Feedback({ params: { title } }: PositionParams) {
+  const divStyle = {
+    width : '75%'
+  };
   return (
     <div className="bg-slate-50 h-full">
       <Navbar />
-      <div className="flex flex-col items-center justify-center h-full">
-        <h1 className="text-4xl font-bold mt-80">Positions</h1>
+      <div className="flex flex-col items-center justify-center h-full">   
+          <div className="h-4 rounded-full bg-gradient-to-r from-green-500 to-blue-500" style={{width: '75%'}}>
+            <span className="absolute inset-0 flex items-center justify-center text-xs font-semibold text-white">75%</span>
+          </div>          
       </div>
     </div>
   );
